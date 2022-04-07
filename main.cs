81d24d8d4e95d4a3787a5da2be535d9a4858c82b
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-04-07 17:12:55 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+// Created by: Lucas Tyman
+// Created on: Mar 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double celsius;
+        double fahreneit;
+
+        // input
+        Console.WriteLine("This program converts fahrenheit to celsius. ");
+        Console.WriteLine("");
+        Console.WriteLine("the formula is: (°F - 32) * 5/9 = 0°C");
+        Console.WriteLine("");
+        Console.WriteLine("please enter the measurements below (℉)");
+        Console.WriteLine("");
+
+        Console.Write("Input fahrenheit:");
+        fahreneit = Convert.ToDouble(Console.ReadLine());
+
+        // process
+        celsius = (fahreneit - 32) * 5/9;
+
+        // output
+        Console.WriteLine("");
+        Console.WriteLine("The fahrenheit in celsius is, " + celsius.ToString("0.00") + "℃");
+        Console.WriteLine("\nDone.");
+    }
+}
